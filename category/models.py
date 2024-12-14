29f627e8ed5e4cc2cd4cd74b607a5325cd240a88
sarojoe10 <baniyasaroj0000@gmail.com>
2024-12-14 13:54:32 +0545

    wokring with customuser model

--- category/models.py
+++ category/models.py
@@ -6,4 +6,11 @@ class Category(models.Model):
     category_name = models.CharField(max_length=100)
     slug = models.CharField(max_length=100,unique=True, blank=True, null=True)
     description = models.TextField(max_length=500, blank=True, null=True)
-    
+    cat_image = models.ImageField(upload_to='images/categories',blank=True,default='default.jpg')
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.category_name

92469cf02a38fb365209cb8f34533c93827eb459
sarojoe10 <baniyasaroj0000@gmail.com>
2024-12-23 00:25:57 +0545

    worked on the cart of the site

--- ecom/urls.py
+++ ecom/urls.py
@@ -24,7 +24,8 @@ from .views import home
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('',home, name='home'),
-    path('store/',include('store.urls'))
+    path('store/',include('store.urls')),
+    path('cart/',include('cart.urls'))
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
